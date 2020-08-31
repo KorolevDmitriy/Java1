1f1773fee77478c0bc377954f4e84e0f470bc4ba
Dmitriy <dkorolev@mail.ru>
2020-08-31 18:57:21 +0300

    homework2

---
+++ src/lesson2/homework/Task2.java
@@ -0,0 +1,17 @@
+package lesson2.homework;
+
+import java.util.Arrays;
+
+public class Task2 {
+
+    public static void main(String[] args) {
+        int[] array = new int[8];
+
+//      for(int i = 0,value =0; i < array.length; i++, value += 3) {
+        for (int i = 0; i < array.length; i++){
+            array[i] = i * 3;
+//          array[i] = value;
+        }
+        System.out.println(Arrays.toString(array));
+    }
+}
