c2fa28d4d1c2f5c2e5eaa23969732a98ea4019c2
Dmitriy <dkorolev@mail.ru>
2020-09-03 14:52:23 +0300

    homework4

--- src/lesson4/TicTacToe.java
+++ src/lesson4/TicTacToe.java
@@ -7,15 +7,17 @@ import java.util.Scanner;
 public class TicTacToe {
 
     private static final char DOT_EMPTY = '•';
-    private static final char DOT_X = '*';
+    private static final char DOT_X = 'X';
     private static final char DOT_O = 'O';
 
-    private static final int SIZE = 3;
-//    private static final int DOTS_TO_WIN = 3;
+    private static final int SIZE = 5;
+    private static final int DOTS_TO_WIN = 4;
 
     private static final char[][] map = new char[SIZE][SIZE];
     private static final Scanner scanner = new Scanner(System.in);
     private static final Random random = new Random();
+    private static final String PLAYER_WIN_MESSAGE = "Пользовательн победил!";
+    private static final String AI_WIN_MESSAGE = "Поедил Искуственный Интеллект!";
 
     public static void main(String[] args) {
         prepareGame();
@@ -27,22 +29,14 @@ public class TicTacToe {
         while (true) {
             humanTurn();
             printMap();
-            if(checkWin(DOT_X)) {
-                System.out.println("Победил человек!");
+            if(checkEnd(DOT_X, PLAYER_WIN_MESSAGE)) {
                 break;
             }
 
             aiTurn();
             printMap();
-            if (checkWin(DOT_O)) {
-                System.out.println("Победил Искуственный Интеллект!");
+            if (checkEnd(DOT_O, AI_WIN_MESSAGE))
                 break;
-            }
-
-            if (isMapFull()) {
-                System.out.println("Ничья!");
-                break;
-            }
         }
     }
 
@@ -62,30 +56,92 @@ public class TicTacToe {
         return true;
     }
 
-    @SuppressWarnings("RedundantIfStatement")
+    private static boolean checkEnd(char symbol, String winMessage) {
+        if (checkWin(symbol)) {
+            System.out.println(winMessage);
+            return true;
+        }
+        if (isMapFull()) {
+            System.out.println("Ничья!");
+            return true;
+        }
+        return false;
+    }
+
     private static boolean checkWin(char symbol) {
-        if (map[0][0] == symbol && map[0][1] == symbol && map[0][2] == symbol) return true;
-        if (map[1][0] == symbol && map[1][1] == symbol && map[1][2] == symbol) return true;
-        if (map[2][0] == symbol && map[1][1] == symbol && map[2][2] == symbol) return true;
+        if (checkRowsAndCols(symbol)) return true;
+        return checkDiagonals(symbol);
+    }
 
-        if (map[0][0] == symbol && map[1][1] == symbol && map[2][0] == symbol) return true;
-        if (map[0][1] == symbol && map[1][1] == symbol && map[2][1] == symbol) return true;
-        if (map[0][2] == symbol && map[1][2] == symbol && map[2][2] == symbol) return true;
+    private static boolean checkRowsAndCols(char symbol) {
+        for (int i = 0; i < SIZE; i++) {
+            int rowCounter = 0;
+            int colCounter = 0;
+            for (int j = 0; j < SIZE; j++) {
+                rowCounter = (map[i][j] == symbol) ? rowCounter + 1 : 0;
+                colCounter = (map[j][i] == symbol) ? colCounter + 1 : 0;
+                if (rowCounter == DOTS_TO_WIN || colCounter == DOTS_TO_WIN) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 
-        if (map[0][0] == symbol && map[1][1] == symbol && map[2][2] == symbol) return true;
-        if (map[0][2] == symbol && map[1][1] == symbol && map[2][0] == symbol) return true;
-
+    private static boolean checkDiagonals(char symbol) {
+        int mainDiagCounter = 0;
+        int sideDiagCounter = 0;
+        for (int  i = 0; i < SIZE; i++) {
+            mainDiagCounter = (map[i][i] == symbol) ? mainDiagCounter + 1 : 0;
+            sideDiagCounter = (map[i][map.length - 1 - i] == symbol) ? sideDiagCounter + 1 : 0;
+            if (mainDiagCounter == DOTS_TO_WIN || sideDiagCounter == DOTS_TO_WIN) {
+                return true;
+            }
+        }
         return false;
     }
 
     private static void aiTurn() {
+        int [] cell = getCellToBlockOpponent(DOT_X);
+        if (cell == null) {
+            cell = getRandomEmptyCell();
+        }
+        int rowIndex = cell[0];
+        int colIndex = cell[1];
+        setCell(rowIndex, colIndex, DOT_O);
+    }
+
+    private static void setCell(int rowIndex, int colIndex, char symbol) {
+        map[rowIndex][colIndex] = symbol;
+    }
+
+        @SuppressWarnings("SamePerameterValue")
+    private static int[] getCellToBlockOpponent(char opponentSymbol) {
+        for (int rowIndex = 0; rowIndex < map.length; rowIndex++) {
+            for (int colIndex = 0; colIndex < map[rowIndex].length; colIndex++) {
+                if (map[rowIndex][colIndex] == DOT_EMPTY && isGameMoveWinning(rowIndex, colIndex, opponentSymbol)) {
+                    return new int[]{rowIndex, colIndex};
+                }
+            }
+        }
+
+        return null;
+    }
+
+    private static boolean isGameMoveWinning(int rowIndex, int colIndex, char opponentSymbol) {
+        setCell (rowIndex, colIndex, opponentSymbol);
+        boolean result = checkWin(opponentSymbol);
+        setCell (rowIndex, colIndex, DOT_EMPTY);
+        return result;
+    }
+
+    private static int[] getRandomEmptyCell() {
         int rowIndex, colIndex;
         do {
             rowIndex = random.nextInt(SIZE);
             colIndex = random.nextInt(SIZE);
-        } while (!isCellValid(rowIndex,colIndex));
-
-        map[rowIndex][colIndex] = DOT_O;
+        } while (!isCellValid(rowIndex, colIndex));
+        return new int[] {rowIndex, colIndex};
     }
 
     private static void humanTurn() {
